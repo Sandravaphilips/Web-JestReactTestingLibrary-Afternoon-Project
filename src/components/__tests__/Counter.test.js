9b10916cf1fe6ec0717b617efb9e7861eb8cab46
Sandrava Philips <lacasandra1@gmail.com>
2019-10-16 14:38:50 +0100

    wrote tests for helpersjs

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import * as rtl from '@testing-library/react';
 import Counter from '../Counter';
